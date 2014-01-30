ffdf9ae1fec74e47a73308017053bddaf2979974
Matt Ericson <mericson@ericson.net>
2014-01-30 11:32:31 -0500

    log

--- index.js
+++ index.js
@@ -18,47 +18,29 @@ module.exports = function(data, opt) {
 
   return es.map(function (file, cb) {
 
-    console.log( file.cwd )
-    console.log( file.base )
-    console.log( file.path )
-    console.log( 'i started here')
+    console.log( 'data-loader' );
+    console.log( 'cwd:  ' + file.cwd );
+    console.log( 'base: ' + file.base );
+    console.log( 'path: ' + file.path );
 
     if ( file.contents ) {
 
     var keyName = opt.stripExt ? file.path.replace(/\.\w+$/,'') : file.path;
-    console.log( 'now i am here')
 
     if ( opt.parseData ) {
-      console.log( 'now i am parsing data here')
 
       var ext = getExtension( file.path );
-      console.log( ext );
 
       if ( ext == 'json' ) {        
-        console.log( 'start json parse')
-
         data[ keyName ] = JSON.parse( file.contents.toString() );
-        console.log( 'end json parse')
-
       } else {
-                console.log( 'somewhere else json parse')
-
          data[ keyName ] = file.contents.toString();
       }
 
-
     } else {
-            console.log( 'now i am not parsing data here')
-
          data[ keyName ] = file.contents.toString();
     }
-    console.log( 'i made it all the way here')
    }
-
-
-    console.log( data );
-
-//    console.log( data )
     cb(null,file);
   });
 };
