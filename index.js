474d08e447d272028874b447c335953ace86951e
Matt Ericson <mericson@ericson.net>
2014-01-30 00:05:49 -0500

    data

---
+++ index.js
@@ -0,0 +1,13 @@
+'use strict';
+var map = require('map-stream');
+var es = require('event-stream');;
+var gutil = require('gulp-util');
+
+module.exports = function(data) {
+  return es.map(function (file, cb) {
+    keyName = file.path.replace(/\.\w+$/,'');
+    data[ keyName ] = file.contents.toString();
+    cb(null,file);
+  });
+};
+
